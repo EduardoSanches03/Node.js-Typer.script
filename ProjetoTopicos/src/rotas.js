68738a4b1c54ca61169cfb8b53687b14895d3d94
Eduardo <duudu_sk61@hotmail.com>
2023-05-17 20:09:12 -0300

    CRUD Cliente feito

--- ProjetoTopicos/src/rotas.js
+++ ProjetoTopicos/src/rotas.js
@@ -2,13 +2,9 @@ const  express = require("express");
 const rotas = express.Router();
 const clienteController = require("./controller/clienteController");
 
-rotas.get("/", (req, res) =>{
-    return res.json({
-    mensagem: "rota raiz",
-    });
-    });
-
 rotas.get('/cliente', clienteController.read);
 rotas.post('/cliente', clienteController.create);
+rotas.delete('/cliente/:id',clienteController.delete);
+rotas.post('/cliente/:id', clienteController.update);
 
 module.exports = rotas;
